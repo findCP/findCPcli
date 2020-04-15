78491d78a2f8e163f686af748ffb3703c1f5d64d
alexOarga <alex718123@gmail.com>
2020-04-15 20:06:57 +0200

    add raise

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="findCPcli",
-    version="0.0.3",
+    version="0.0.4",
     author="Alex Oarga",
     author_email="alex718123@gmail.com",
     description="findCP CLI package",
