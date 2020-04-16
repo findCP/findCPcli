e7192683aadad6c3b1430ada15621044e9278fa5
alexOarga <alex718123@gmail.com>
2020-04-16 20:51:55 +0200

    remove raise exception

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="findCPcli",
-    version="0.0.4",
+    version="0.0.5",
     author="Alex Oarga",
     author_email="alex718123@gmail.com",
     description="findCP CLI package",
